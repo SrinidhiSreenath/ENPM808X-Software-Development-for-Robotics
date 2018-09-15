52f9e0e89ec4418c88c09b66d9d663e44b1f3505
Srinidhi Sreenath <sreenath.srinidhi@gmail.com>
2018-09-15 19:35:55 -0400

    Reorganize code according to google c++ style

--- Week 3/SoftwareEngineering_9_4/app/main.cpp
+++ Week 3/SoftwareEngineering_9_4/app/main.cpp
@@ -1,4 +1,5 @@
 /**
+ *  Copyright 2018 Srinidhi Sreenath
  *  @file    main.cpp
  *  @author  Srinidhi Sreenath (SrinidhiSreenath)
  *  @date    9/15/2018
@@ -11,9 +12,11 @@
  *  This file contains the code to extract a new function that returns 
  *  the position of the beginning of a given string in a given text.
  *
+ *
+ *
  */
 
-//CPP Headers
+// CPP Headers
 #include <iostream>
 #include <string>
 
@@ -24,12 +27,13 @@
  *   @param  stringToSearch is string to be searched in the text
  *   @return start position of string in text as size_t
  */
-size_t getPositionInText(const std::string& text, const std::string& stringToSearch){
+size_t getPositionInText(const std::string& text,
+                         const std::string& stringToSearch) {
     // iterate through the text string
-  for(size_t i=0; i < text.size(); i++){
+  for (size_t i = 0; i < text.size(); i++) {
     bool found = true;  ///< flag to check if string is in text
-    for(size_t j=0; j < stringToSearch.size(); j++){
-      if(text[i+j] != stringToSearch[j]){
+    for (size_t j = 0; j < stringToSearch.size(); j++) {
+      if (text[i + j] != stringToSearch[j]) {
         found = false;  // flag is false if the entire string isn't
                         // in position when start of string is found in text
       }
@@ -48,21 +52,18 @@ size_t getPositionInText(const std::string& text, const std::string& stringToSea
  *   @param  stringToSearch is string to be searched in the text
  *   @return void
  */
-void printPosition(const std::string& text, const std::string& stringToSearch){
-
-  size_t position = getPositionInText(text, stringToSearch); //Function call to get position of string in text
+void printPosition(const std::string& text, const std::string& stringToSearch) {
+  size_t position = getPositionInText(text, stringToSearch);  // Function call
 
   std::cout << "The position of the string: " << stringToSearch
             << " in the text: " << text << " is " << position << std::endl;
 }
- 
 
-int main()
-{
-  std::string text = "Srinidhi";  ///< text string that contains the string to be searched for
+int main() {
+  std::string text = "Srinidhi";  ///< text string
   std::string stringToSearch = "idhi";  ///< string to search for in text string
 
-  printPosition(text, stringToSearch);  // Function call that prints the position of the string in the text
+  printPosition(text, stringToSearch);  // Function call to print position
 
   return 0;
 }
