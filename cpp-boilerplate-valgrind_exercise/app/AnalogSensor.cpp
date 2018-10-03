3ba788805702507f7d66f6c057949b318d97ed1c
Srinidhi Sreenath <sreenath.srinidhi@gmail.com>
2018-10-02 20:13:21 -0400

    Bugfix: deallocate memory for readings vector

--- cpp-boilerplate-valgrind_exercise/app/AnalogSensor.cpp
+++ cpp-boilerplate-valgrind_exercise/app/AnalogSensor.cpp
@@ -16,6 +16,7 @@ int AnalogSensor::Read()
     std::vector<int> *readings = new std::vector<int>(mSamples, 10);
 
     double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
+    delete readings;
     return result;
 }
 
