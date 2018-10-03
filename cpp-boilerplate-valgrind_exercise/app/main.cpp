6ec37a3d6ff94c9572ccd9e97f3a128b1fc348da
Srinidhi Sreenath <sreenath.srinidhi@gmail.com>
2018-10-02 20:03:54 -0400

    Bugfix uninitialized boolean variable

--- cpp-boilerplate-valgrind_exercise/app/main.cpp
+++ cpp-boilerplate-valgrind_exercise/app/main.cpp
@@ -5,7 +5,7 @@ int main()
 {
     AnalogSensor lightSensor(5);
     std::cout << "Averaged sensor reading: " << lightSensor.Read() << std::endl;
-    bool terminator;
+    bool terminator = true;
     if( terminator )
     {
         std::cout << "DONE" << std::endl;
